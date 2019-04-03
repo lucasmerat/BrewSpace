5c8559856e288e8bdf4967d2e4966ce01c9561e2
Lucas Merat <lucas.merat@gmail.com>
2019-04-03 11:44:12 -0500

    Added working signin with route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Load index page
+  // Load landing page
   app.get("/", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
@@ -11,9 +11,15 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/signin", function(req, res) {
+    res.render("signin");
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
